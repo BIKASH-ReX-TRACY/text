e16b2dbb1631051f485f82e583b0c03b78b48346
BIKASH-ReX-TRACY <130630071+BIKASH-ReX-TRACY@users.noreply.github.com>
2023-05-01 19:16:52 +0545

    Create r.py

---
+++ r.py
@@ -0,0 +1,31 @@
+#created by xerx aka Aryan chaudhary
+
+#open source by xerx
+
+#github= XERX-XD
+
+#BY BY MEY KABHI WAPIS NAHI AAUNGA AB 
+
+#JAI NEPAL
+
+# fb id https://www.facebook.com/aaryan.chaudhary69
+
+from bs4 import BeautifulSoup as sop
+
+from concurrent.futures import ThreadPoolExecutor as tred
+
+import os,sys,time,json,random,re,string,platform,base64,platform,uuid
+
+import requests,random,sys,json,os,re
+
+from time import sleep
+
+from os import system
+
+import os,sys,time,datetime,random,hashlib,re,threading,json,urllib,uuid,ipaddress,calendar,requests,mechanize,bs4,sys,os,subprocess,uuid,requests,sys,random,time,re,base64,json,platform
+
+import marshal
+
+import zlib
+
+import base64
